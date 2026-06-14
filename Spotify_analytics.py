993366d615e493fb4e5f914c8cf063db9e2e827e
Ojanko123 <ojanko11@outlook.com>
2026-06-14 14:11:33 +0200

    Update Spotify_analytics.py

--- Spotify_analytics.py
+++ Spotify_analytics.py
@@ -31,7 +31,7 @@ from scipy import stats
 import warnings
 warnings.filterwarnings('ignore')
 
-_dir = os.path.dirname(os.path.abspath(_file_))
+_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Seaborn style
 sns.set_theme(style='whitegrid', palette='husl')
